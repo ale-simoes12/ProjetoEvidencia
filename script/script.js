5f9f4383f09066466f6f5e6904fe7ebe4ee03a50
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-03-30 19:20:55 -0300

    adicionando tela shop

---
+++ script/script.js
@@ -0,0 +1,5 @@
+function esconderDiv() {
+    console.log("clickiu");
+    document.querySelector('.makeblock').style.display = 'none';
+    document.querySelector('.produtos-naovisiveis').style.display = 'block';
+}
