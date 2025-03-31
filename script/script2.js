1f34bc4aad923a23c0795405b380a4511c3a85ca
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-03-31 17:07:02 -0300

    adicionando animacoes

---
+++ script/script2.js
@@ -0,0 +1,17 @@
+document.addEventListener('mousemove', function(e) {
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+  
+    
+    const bola1 = document.querySelector('.bola1');
+    bola1.style.left = `${mouseX}px`;
+    bola1.style.top = `${mouseY}px`;
+  
+    // Bola 2 (com atraso)
+    const bola2 = document.querySelector('.bola2');
+    setTimeout(() => {
+      bola2.style.left = `${mouseX}px`;
+      bola2.style.top = `${mouseY}px`;
+    }, 150);
+  });
