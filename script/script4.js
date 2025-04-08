137fae3f8074350e8740ab674915ac1e40828e59
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-04-07 22:18:10 -0300

    animacao nas imagens da galeria usando o mouse

---
+++ script/script4.js
@@ -0,0 +1,25 @@
+const area = document.getElementById('area-bola');
+const bola = document.getElementById('bola-azul');
+const bola1 = document.querySelector('.bola1');
+const bola2 = document.querySelector('.bola2');
+
+area.addEventListener('mouseenter', () => {
+  bola.style.display = 'block';  
+  bola1.style.display = 'none';   
+  bola2.style.display = 'none';    
+});
+
+area.addEventListener('mouseleave', () => {
+  bola.style.display = 'none';    
+  bola1.style.display = 'block';  
+  bola2.style.display = 'block';   
+});
+
+area.addEventListener('mousemove', (e) => {
+  const rect = area.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  bola.style.left = `${x}px`;
+  bola.style.top = `${y}px`;
+});
